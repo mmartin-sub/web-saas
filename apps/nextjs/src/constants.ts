0ae82249b1500c9d4757fa9c6ddb4171bb2c8221
Rob <33225407+shuther@users.noreply.github.com>
2024-08-15 17:32:56 +0200

    add new constant file

---
+++ apps/nextjs/src/constants.ts
@@ -0,0 +1,41 @@
+/**
+ * The base URL for the discord server.
+ * @constant
+ */
+export const DISCORD_SERVER = 'https://discord.gg/nata9PpP';
+
+/**
+ * If the section that is showing the discord page should be hidden
+ * @constant
+ */
+export const HIDE_DISCORD_LINK = true;
+
+/**
+ * If the section that is showing the WORD section should be hidden
+ * @constant
+ */
+export const HIDE_WORD_SECTION = false
+
+/**
+ * If the section that is showing the Questions section should be hidden
+ * @constant
+ */
+export const HIDE_QUESTIONS_SECTION = false
+
+/**
+ * Hide the sentence next to the WORDS section with a special effect if we scroll down
+ * @constant
+ */
+export const HIDE_WORD_REVEAL = false
+
+/**
+ * Hide the Blog section in the homepage
+* @constant
+ */
+export const HIDE_BLOG_HOMEPAGE = false
+
+/**
+ * Hide the Featurecard section in the homepage - more notifications
+* @constant
+ */
+export const HIDE_FEATURECARD_HOMEPAGE = false
