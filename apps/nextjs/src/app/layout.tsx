5aab4afa43db5b1787e417cbd8a76131b5bfc6ca
Rob <33225407+shuther@users.noreply.github.com>
2024-08-16 08:58:29 +0200

    cleanup previous experimentaion

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -77,7 +77,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning >
       <head />
       {/*<Suspense>*/}
       {/*  <PostHogPageview />*/}
