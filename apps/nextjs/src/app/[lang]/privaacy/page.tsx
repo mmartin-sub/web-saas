e7d56fdee45056dc6a523065b588bff877079888
Rob <33225407+shuther@users.noreply.github.com>
2024-08-16 18:11:54 +0200

    privacy/privaacy page working but no md(x) - commit before removal

---
+++ apps/nextjs/src/app/[lang]/privaacy/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+export default function PrivaacyPage() {
+    // const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+
+    return (
+      <div className="container flex h-screen w-screen flex-col items-center justify-center">
+    Test Privacy 2
+      </div>
+    );
+  }
